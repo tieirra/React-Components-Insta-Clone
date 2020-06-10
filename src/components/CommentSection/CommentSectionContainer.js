44b88ff0bb316dcb545b691de8a3cb42f163899e
user <user@users-MacBook-Air.local>
2020-06-10 09:52:53 -0500

    adding .map to return comment component

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,16 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+const[ comments , setComments] = useState;
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {/* map through the comments data and return the Comment component */
+      CommentSection.comments.map(
+       Comment => {
+       return Comment;
+       }) 
+      }
       <CommentInput />
     </div>
   );
